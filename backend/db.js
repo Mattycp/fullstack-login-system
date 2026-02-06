f0ff2bfd74709f0f3da0951be196cf1d59035e1b
Mattycp <matycp17@gmail.com>
2026-02-05 22:20:23 -0300

    correções banco de dados

--- backend/db.js
+++ backend/db.js
@@ -5,5 +5,6 @@ module.exports = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT
 });
